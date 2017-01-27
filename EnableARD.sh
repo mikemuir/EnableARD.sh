6a71f454e0098d298cfd688756be715890345409
mikemuir <mike@muirsandwich.com>
2017-01-26 17:13:54 -0800

    EnableARD.sh
    These commands enable ARD via command line script to first enable the
    Remote Management service, then give full control rights for the
    defined ‘adminshortname’ user.

---
+++ EnableARD.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Enable ARD Settings
+
+#Enable ARD for Specific Users
+/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -specifiedUsers
+#Enable ARD Agent for CRADMIN
+/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -users adminshortname -privs -all -restart -agent -menu
