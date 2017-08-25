5b613a32bc637410a0f6b0dc662ef1c9c820fe65
mikemuir <mike@creativeresources.net>
2017-08-24 17:08:20 -0700

    Update EnableARD.sh

--- EnableARD.sh
+++ EnableARD.sh
@@ -3,5 +3,5 @@
 
 #Enable ARD for Specific Users
 /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -specifiedUsers
-#Enable ARD Agent for CRADMIN
+#Enable ARD Agent for <adminshortname>
 /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -users adminshortname -privs -all -restart -agent -menu
